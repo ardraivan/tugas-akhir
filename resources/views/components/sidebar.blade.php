d69f35978d2b86cdfaefece747bc921938684c0d
ardraivan <ardraivand@gmail.com>
2024-08-27 11:50:25 +0700

    revisi sidebar

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -39,24 +39,10 @@
               <p>Laporan Pemeliharaan</i></p>
             </a>
           </li>
-          <li class="nav-item">
-            <a href="#" class="nav-link">
-              <i class="nav-icon fas fa-tree"></i>
-              <p>Pengaturan<i class="fas fa-angle-left right"></i></p>
-            </a>
-            <ul class="nav nav-treeview">
-              <li class="nav-item">
-                <a href="pages/UI/general.html" class="nav-link">
-                  <i class="far fa-circle nav-icon"></i>
-                  <p>Kelola Profil</p>
-                </a>
-              </li>
-            </ul>
-          </li>
-          <li class="nav-item">
+          {{-- <li class="nav-item">
             <a href="#" class="nav-link">
               <i class="nav-icon fas fa-edit"></i>
-              <p>Logout</p>
+              <p>Logout</p> --}}
             </a>
           </li>
         </ul>
